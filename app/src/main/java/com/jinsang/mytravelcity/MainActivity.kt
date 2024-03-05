a849561e0d5aabf9d436e75b4c5601741ebc59d0
jinsang <jinsang@nhndata.com>
2024-03-05 14:06:44 +0900

    catalog libs를 이용해서 androidx-material3-window-size-class-android 라이브러리 참조

--- app/src/main/java/com/jinsang/mytravelcity/MainActivity.kt
+++ app/src/main/java/com/jinsang/mytravelcity/MainActivity.kt
@@ -7,12 +7,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.jinsang.mytravelcity.ui.theme.MyTravelCityTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,6 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    val windowSize = calculateWindowSizeClass(this)
                     Greeting("Android")
                 }
             }
